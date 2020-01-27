65431810da5286b3a15ea9ff5f088839927a11ee
FIrst <FIrst@192.168.21.165>
2020-01-27 16:32:53 +0100

    In UtenteDao aggiunti Throws.

--- src/main/java/it/accenture/dao/UtenteDao.java
+++ src/main/java/it/accenture/dao/UtenteDao.java
@@ -1,5 +1,6 @@
 package it.accenture.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import exceptions.DAOException;
@@ -7,14 +8,14 @@ import it.accenture.model.Utente;
 
 public interface UtenteDao {
 
-	public void insertUtente(Utente utente) throws DAOException;
+	public void insertUtente(Utente utente) throws DAOException, SQLException;
 	
 	public Utente getByUsernameAndPassword(String username, 
-			String password);
+			String password) throws SQLException;
 	
-	public void updateUtente(Utente utente);
+	public void updateUtente(Utente utente) throws SQLException;
 	
-	public void deleteteUtenteById(int idutente);
+	public void deleteteUtenteById(int idutente) throws SQLException;
 	
 	public void close();
 	
