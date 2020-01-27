199da96df7e65c5df52bda02e3a330d6bc7608c4
first <first@192.168.21.107>
2020-01-27 15:58:32 +0100

    prova di cose per vedere se funziona.

--- src/main/java/exceptions/ConnessioneException.java
+++ src/main/java/exceptions/ConnessioneException.java
@@ -1,6 +1,10 @@
 package exceptions;
 
 public class ConnessioneException extends Exception {
+	
+	public ConnessioneException() {
+		super("PROBLEMI DI CONNESSIONE");
+	}
 
 	public ConnessioneException(String message) {
 		super(message);	
--- src/main/java/exceptions/DAOException.java
+++ src/main/java/exceptions/DAOException.java
@@ -1,6 +1,10 @@
 package exceptions;
 
 public class DAOException extends Exception {
+	
+	public DAOException() {
+		super("Errore nel DAO.");
+	}
 
 	public DAOException(String message) {
 		super(message);
