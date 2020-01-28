d4f0914a06a6454858be370a696fdd54c847f3ab
first <first@first-PC>
2020-01-28 09:40:25 +0100

    stanza dao+bean

--- src/main/java/it/accenture/dao/StanzaDao.java
+++ src/main/java/it/accenture/dao/StanzaDao.java
@@ -1,16 +1,19 @@
 package it.accenture.dao;
 
-import java.util.List;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import exceptions.ConnessioneException;
+import exceptions.DAOException;
 import it.accenture.model.Stanza;
 
 public interface StanzaDao {
 
-	public List<Stanza> getAll();
+	public ArrayList<Stanza> getAll() throws DAOException, ConnessioneException, SQLException;
 	
-	public void updateDisponibile(boolean disponibile, int numeroStanza);
+	public void updateDisponibile(boolean disponibile, int numeroStanza) throws DAOException, ConnessioneException, SQLException;
 	
-	public Stanza getStanzaById(int numeroStanza);
+	public Stanza getStanzaById(int numeroStanza) throws DAOException, ConnessioneException, SQLException;
 	
 	public void close();
 	
