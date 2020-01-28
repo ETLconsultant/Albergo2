4cb248d291a0381547f440c1f9fa66d23205c553
first <first@first-PC>
2020-01-28 15:43:25 +0100

    ok

--- src/main/java/it/accenture/service/PrenotazioneService.java
+++ src/main/java/it/accenture/service/PrenotazioneService.java
@@ -1,5 +1,6 @@
 package it.accenture.service;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,23 +16,23 @@ public class PrenotazioneService {
 	
 	private PrenotazioneDaoImpl PrenotazioneDao = new PrenotazioneDaoImpl();
 	
-	public void insertPrenotazione(Prenotazione prenotazione) {
+	public void insertPrenotazione(Prenotazione prenotazione) throws SQLException {
 		PrenotazioneDao.insertPrenotazione(prenotazione);
 	}
 	
-	public List<Prenotazione> getAllByUtente(int idUtente) {
+	public List<Prenotazione> getAllByUtente(int idUtente) throws SQLException {
 		return PrenotazioneDao.getAllByUtente(idUtente);
 	}
 	
-	public List<Periodo> getPeriodiByStanza(int numeroStanza) {
+	public List<Periodo> getPeriodiByStanza(int numeroStanza) throws SQLException {
 		return PrenotazioneDao.getPeriodiByStanza(numeroStanza);
 	}
 	
-	public boolean controlloDate(LocalDate dataInizio, LocalDate dataFine, int numeroStanza) {
+	public boolean controlloDate(LocalDate dataInizio, LocalDate dataFine, int numeroStanza) throws SQLException {
 		return PrenotazioneDao.controlloDate(dataInizio, dataFine, numeroStanza);
 	}
 	
-	public void controlloDisponibilitaQuotidiana(List<Stanza> listaStanze) {
+	public void controlloDisponibilitaQuotidiana(List<Stanza> listaStanze) throws SQLException {
 		PrenotazioneDao.controlloDisponibilitaQuotidiana(listaStanze);
 		
 	}
