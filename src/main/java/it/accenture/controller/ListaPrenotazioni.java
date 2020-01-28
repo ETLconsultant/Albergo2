111e8350301871b66dc376cfe2c33e44bb0a065f
First <First@First-PC>
2020-01-28 11:41:32 +0100

    nicola

--- src/main/java/it/accenture/controller/ListaPrenotazioni.java
+++ src/main/java/it/accenture/controller/ListaPrenotazioni.java
@@ -14,15 +14,32 @@ import javax.servlet.http.HttpSession;
 
 import exceptions.ConnessioneException;
 import it.accenture.dao.PrenotazioneDaoImpl;
+import it.accenture.dao.UtenteDaoImpl;
 import it.accenture.model.Prenotazione;
 import it.accenture.model.Utente;
+import it.accenture.service.PrenotazioneService;
 
 @WebServlet("/listaPrenotazioni")
 public class ListaPrenotazioni extends HttpServlet {
+	String messagePrenot = " ";
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		Utente u1 = (Utente) session.getAttribute("utente");
+		
 	
+		PrenotazioneService prenot = new PrenotazioneService();
+		
+		prenot.getAllByUtente(u1.getIdUtente());
+		
+		messagePrenot = ("Gentile "+ u1.getNome() + " " + u1.getCognome() + ", questa e' la tua "
+				+ "lista di prenotazioni effettuate: ");
+		
+		session.setAttribute("messagePrenot", messagePrenot);
+		RequestDispatcher rd = req.getRequestDispatcher("/listaPrenotazioni.jsp");
+		rd.forward(req, resp);
 	
   }
 	
