6aac7e0eb6a5ff2692739558f6c6b9164e06a0e6
first <first@first-PC>
2020-01-28 12:38:09 +0100

    servlet login fatta

--- src/main/java/it/accenture/controller/Login.java
+++ src/main/java/it/accenture/controller/Login.java
@@ -1,6 +1,7 @@
 package it.accenture.controller;
-
+//ghijuoikpl
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
@@ -11,16 +12,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
+
 import exceptions.ConnessioneException;
 import it.accenture.dao.UtenteDaoImpl;
 import it.accenture.model.Utente;
 
 @WebServlet("/login")
 public class Login extends HttpServlet {
+	private static final long seriaHtlVersionUID = 1L;
+	private Utente u=new Utente();
+	
+	public Login() {
+        super();
+    }
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session=req.getSession();
+		String username=req.getParameter("username");
+		String password=req.getParameter("password");
+		String regexP ="^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[#$^+=!*()@%&]).{8,}";
+		String regexE ="\\w+([\\.-]?\\w+)@\\w+([\\.-]?\\w+)(\\.\\w{2,3})+}";
 		
-	}
+		if(u.getUsername()!=null) {
+			if((u.getPassword().equalsIgnoreCase(password))) {
+				session.setAttribute("username", username );
+				session.setAttribute("password", password );
+				session.setAttribute("idUtente", u.getId());
+				session.setAttribute("nome", u.getNome());
+					RequestDispatcher rd= req.getRequestDispatcher("/login.jsp");
+					rd.forward(req, resp);
+			}
+			else{
+				String messaggio="password errata";
+				req.setAttribute("msg", messaggio);
+				RequestDispatcher rd= req.getRequestDispatcher("/index.jsp");
+				rd.include(req, resp);
+			}
+		}else {
+			String messaggio="utente inesistente";
+			req.setAttribute("msg", messaggio);
+			RequestDispatcher rd= req.getRequestDispatcher("/index.jsp");
+			rd.include(req, resp);
+			
+		}
 	
+	}
 }
